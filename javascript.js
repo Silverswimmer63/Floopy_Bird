aa31cb5428418a3b8a9f46e9a69c9d07c5311925
DeadlyNinja1201 <47150010+DeadlyNinja1201@users.noreply.github.com>
2021-01-12 12:59:45 -0500

    collition for top pipe bottom
    Its a little jangky still but it will look better ounce we get it to pause as soon as it hits the walls I could not get it to on my code because it was too slow to but i will see if its better with some better frames and such but ill have todo that later or something but i got comments done here as well :)

--- javascript.js
+++ javascript.js
@@ -1,23 +1,46 @@
+
+//Commenting note
+// @thing  : start of declarations with thing saying what it is variable,object,class and so on
+// [] : tells what it is/ what it is for
+// {} : restrictions, info, types, ect.
+// {restricted} : states that the said thing is restricted in some way shape or form
+
+//@variable c [Object] : Canvas elements
 var c = document.getElementById("myCanvas");
+//@variable ctx [object] : Canvas elements 2d
 var ctx = c.getContext("2d");
 
+//@variable ball [object] : ball position and restrictions
 var ball = {xPos: c.width/20, yPos: c.height/20, xMove: 5, yMove: 5, rad: 20};
 
+//@variable gravity [integer] {retricted :>1} : Gravity control 
 var gravity = 0.2;
-var damping = 0.75;
-var rad = 20;
 
+//@variable damping [integer] {restricted: >1} : changes the frictional forces on the x value
+var damping = 0.75;
+
+//@variable rad [integer] : radius of the ball
+var rad = 20; 
+
+//function drawCircle();
+//@purpose [draw] : draws and refreshes the ball/birb
 function drawCircle() {
-  ctx.beginPath();
-  ctx.arc(ball.xPos, ball.yPos, rad, 0, Math.PI*2);
-  ctx.fillStyle = "red";
-  ctx.fill();
-  ctx.stroke();
+  ctx.beginPath();//beggins dawing
+  ctx.arc(ball.xPos, ball.yPos, rad, 0, Math.PI*2);//starts arc for ball
+  ctx.fillStyle = "red";// changes the collor to red
+  ctx.fill();//sets the fill
+  ctx.stroke();//ends drawing
 }
 
+//@variable rectWidth [integer:random] {restricted} : rectangele width raqndomizer
 var rectWidth = Math.floor(Math.random() * (150 - 100) + 100);
-var rectHeight = Math.floor(Math.random() * (175 - 125) + 125);
+
+//@variable rectHeight [integer:random] {restricted} : rectangle height randomizer
+var rectHeight = Math.floor(Math.random() * (190 - 90) + 90);
+
+//@variable rect [object] {requires:c[variable],rectWidth[variable],rectHeight[variable]} : makes an object fpr the rectangles that make the pipes;
 var rect = {xPos: c.width-rectWidth, yPos: c.height-rectHeight, width: rectWidth, height: rectHeight};
+
 /*
 Use this for gradient coloring of the tubes
 var grd = ctx.createLinearGradient(0, 0, 200, 0);
@@ -26,77 +49,92 @@ grd.addColorStop(1, "white");
 ctx.fillStyle = grd;
 ctx.fillRect(rect.xPos, rect.yPos, rect.width, rect.height);
 */
+
+//function makePipe();
+//@purpose [draw] : draws and refreshes the pipes
   function makePipe(){
     ctx.clearRect(0, 0, c.width, c.height); //since it's a loop, this clears the canvas or else a lot of circles will be draw each time this function loops
     //code for the bottom, long pipe
-    ctx.beginPath(); //starts drawing the rectangle
+    ctx.beginPath(); //starts the draw/path
     ctx.rect(rect.xPos, rect.yPos, rect.width, rect.height);
-    ctx.fillStyle = "green"; //Sets the color of the circle to light blue.
-    ctx.fill(); //Fills in the circle with the color provided in fillStyle.
+    ctx.fillStyle = "green"; //Sets the color to green.
+    ctx.fill(); //Fills with the color provided in fillStyle.
     ctx.stroke(); //finish drawing the rectangle
     //code for the top of the pipe on the bottom of the screen
     ctx.beginPath(); //starts drawing the rectangle
     ctx.rect(rect.xPos - 15, rect.yPos-20, rect.width+30, 40);
-    ctx.fillStyle = "green"; //Sets the color of the circle to light blue.
-    ctx.fill(); //Fills in the circle with the color provided in fillStyle.
+    ctx.fillStyle = "green"; //Sets the color to green.
+    ctx.fill(); //Fills it with the color provided in fillStyle.
     ctx.stroke(); //finish drawing the rectangle
     //code for the long pipe moving on the top of the canvas
     ctx.beginPath(); //starts drawing the rectangle
     ctx.rect(rect.xPos, 0, rect.width, rect.height);
-    ctx.fillStyle = "green"; //Sets the color of the circle to light blue.
-    ctx.fill(); //Fills in the circle with the color provided in fillStyle.
+    ctx.fillStyle = "green"; //Sets the color to green.
+    ctx.fill(); //Fills it in with the color provided in fillStyle.
     ctx.stroke(); //finish drawing the rectangle
     //code for
     ctx.beginPath(); //starts drawing the rectangle
     ctx.rect(rect.xPos - 15, rect.height, rect.width+30, 40);
-    ctx.fillStyle = "green"; //Sets the color of the circle to light blue.
-    ctx.fill(); //Fills in the circle with the color provided in fillStyle.
+    ctx.fillStyle = "green"; //Sets the color to green.
+    ctx.fill(); //Fills it in with the color provided in fillStyle.
     ctx.stroke(); //finish drawing the rectangle
-    rect.xPos --;
+    rect.xPos --;//moves the pipes to the west
     if ((rect.xPos + rect.width) == 0) {
-      rect.xPos = c.width;
+      rect.xPos = c.width;//if it is off the canvas draw it on the opposites side
+      //@note [change] {When : " pipes are turned into a class "} : this will instead just nuke them 
     }
   }
 
+
+//function draw()
+//@purpose [handeler] {handels : makePipe() [function], drawCircle() [function],clearing, ball updates}
 function draw() {
-  ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
-  makePipe();
-  drawCircle();
+  ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);//clears to allow clean updates
+  makePipe();//draws pipes
+  drawCircle();//draws birb/ball
   if (ball.xPos + ball.xMove > c.width - rad || ball.xPos + ball.xMove < rad) {
-    ball.xMove = -ball.xMove;
+    ball.xMove = -ball.xMove;//changes ball/birb position x updator
   }
   if (ball.yPos + ball.yMove > c.height - rad || ball.yPos + ball.yMove < rad) {
-    ball.yMove = -ball.yMove * damping;
+    ball.yMove = -ball.yMove * damping;//changes ball y position updator
   }
-  ball.yMove += gravity;
-  ball.xPos += ball.xMove;
+  ball.yMove += gravity;//adds the gravity into the equation
+  ball.xPos += ball.xMove;//makes the ball move around 
   if (((ball.yPos + ball.yMove) + rad) <= c.height) {
-    ball.yPos += ball.yMove;
+    ball.yPos += ball.yMove; //Wall collition dirrection change
   }
   if ((ball.xPos + ball.xMove + rad > rect.xPos) && (ball.yPos + rad < rect.height)) {
-    ball.xMove = -ball.xMove;
+    ball.xMove = -ball.xMove;//wall collition direction change
   }
   if ((ball.xPos + ball.xMove + rad > rect.xPos) && (ball.yPos + rad < rect.height)) {
-    ball.xMove = -ball.xMove;
+    ball.xMove = -ball.xMove;//wall collition direction change
   }
   if ((ball.xPos + ball.xMove + rad > rect.xPos) && (ball.yPos + rad > rect.yPos)) {
-    ball.xMove = -ball.xMove;
+    ball.xMove = -ball.xMove;//wall collition direction chaneg
   }
   if ((ball.xPos + ball.xMove + rad > rect.xPos) && (ball.yPos + rad > rect.yPos)) {
-    ball.xMove = -ball.xMove;
+    ball.xMove = -ball.xMove;//wall collition direction change
   }
   if ((ball.yPos + ball.yMove + rad > rect.yPos) && (ball.xPos + rad < rect.width + rect.xPos) && (rad + ball.xPos > rect.xPos)) { //collides with the top of the bottom pipe
-    ball.yMove = -ball.yMove;
+    ball.yMove = -ball.yMove;//wall collition direction change
+  }
+
+  
+  if ((ball.yPos + ball.yMove + rad < rect.height+50) && (ball.xPos + rad < rect.width + rect.xPos) && (rad + ball.xPos > rect.xPos)) { //collides with the top of the bottom pipe
+    ball.yMove = -ball.yMove;//wall collition direction change
+    console.log("top?")
   }
 }
-setInterval(draw, 10);
+setInterval(draw, 10);//draw() [Interval : start] {type:function Call, time:10ms}
 
-document.addEventListener("keydown", makeBounce);
+//event handeler [keydown] {function : makeBounce()}
+document.addEventListener("keyup", makeBounce);
 function makeBounce(e) {
+  //e.key [string] {space_bar : " ", r_key: "r"};
   if (e.key == " ") {
-    ball.yMove -= 10;
+    ball.yMove -= 5;//adding to the change in the y position to make the ball jump
   }
   if (e.key == "r") {
-    ball.xMove = -ball.xMove;
+    ball.xMove = -ball.xMove;//changing the direction of the x value
   }
 }
