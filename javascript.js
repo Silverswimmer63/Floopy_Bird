b4416ce99182a48ecebf46c84908d01ab9017928
mandy <mwolchetkie@yahoo.com>
2021-01-27 13:18:15 -0500

    my code for Floopy bird game- which does not work, but it did

--- javascript.js
+++ javascript.js
@@ -1,102 +1,114 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
+var bird;//the bird!
+var pipes = [];//the pipes!!
 
-var ball = {xPos: c.width/20, yPos: c.height/20, xMove: 5, yMove: 5, rad: 20};
-
-var gravity = 0.2;
-var damping = 0.75;
-var rad = 20;
-
-function drawCircle() {
-  ctx.beginPath();
-  ctx.arc(ball.xPos, ball.yPos, rad, 0, Math.PI*2);
-  ctx.fillStyle = "red";
-  ctx.fill();//hello
-  ctx.stroke();
+function setup() {
+  createCanvas(640, 480);//black background canvas
+  bird = new Bird();
+  pipes.push(new Pipe());
 }
 
-var rectWidth = Math.floor(Math.random() * (150 - 100) + 100);
-var rectHeight = Math.floor(Math.random() * (175 - 125) + 125);
-var rect = {xPos: c.width-rectWidth, yPos: c.height-rectHeight, width: rectWidth, height: rectHeight};
-/*
-Use this for gradient coloring of the tubes
-var grd = ctx.createLinearGradient(0, 0, 200, 0);
-grd.addColorStop(0, "green");
-grd.addColorStop(1, "white");
-ctx.fillStyle = grd;
-ctx.fillRect(rect.xPos, rect.yPos, rect.width, rect.height);
-*/
-  function makePipe(){
-    ctx.clearRect(0, 0, c.width, c.height); //since it's a loop, this clears the canvas or else a lot of circles will be draw each time this function loops
-    //code for the bottom, long pipe
-    ctx.beginPath(); //starts drawing the rectangle
-    ctx.rect(rect.xPos, rect.yPos, rect.width, rect.height);
-    ctx.fillStyle = "green"; //Sets the color of the circle to light blue.
-    ctx.fill(); //Fills in the circle with the color provided in fillStyle.
-    ctx.stroke(); //finish drawing the rectangle
-    //code for the top of the pipe on the bottom of the screen
-    ctx.beginPath(); //starts drawing the rectangle
-    ctx.rect(rect.xPos - 15, rect.yPos-20, rect.width+30, 40);
-    ctx.fillStyle = "green"; //Sets the color of the circle to light blue.
-    ctx.fill(); //Fills in the circle with the color provided in fillStyle.
-    ctx.stroke(); //finish drawing the rectangle
-    //code for the long pipe moving on the top of the canvas
-    ctx.beginPath(); //starts drawing the rectangle
-    ctx.rect(rect.xPos, 0, rect.width, rect.height);
-    ctx.fillStyle = "green"; //Sets the color of the circle to light blue.
-    ctx.fill(); //Fills in the circle with the color provided in fillStyle.
-    ctx.stroke(); //finish drawing the rectangle
-    //code for
-    ctx.beginPath(); //starts drawing the rectangle
-    ctx.rect(rect.xPos - 15, rect.height, rect.width+30, 40);
-    ctx.fillStyle = "green"; //Sets the color of the circle to light blue.
-    ctx.fill(); //Fills in the circle with the color provided in fillStyle.
-    ctx.stroke(); //finish drawing the rectangle
-    rect.xPos --;
-    if ((rect.xPos + rect.width) == 0) {
-      rect.xPos = c.width;
+function draw() {
+  background(0);
+
+  for (var i = pipes.length - 1; i >= 0; i--) {//length of pipes as
+    pipes[i].show();
+    pipes[i].update();
+
+    if (pipes[i].hits(bird)) {
+      console.log('HIT');//this determines if the pipe has hit the bird "HIT"
+    }
+
+    if (pipes[i].offscreen()) {//this is getting rid of the pipes as they go offscreen (which is a self written function)
+      pipes.splice(i, 1); //splice deletes the element from the array, in this case it is deleting the pipes as they go offscreen
     }
   }
 
-function draw() {
-  ctx.clearRect(0, 0, myCanvas.width, myCanvas.height);
-  makePipe();
-  drawCircle();
-  if (ball.xPos + ball.xMove > c.width - rad || ball.xPos + ball.xMove < rad) {
-    ball.xMove = -ball.xMove;
-  }
-  if (ball.yPos + ball.yMove > c.height - rad || ball.yPos + ball.yMove < rad) {
-    ball.yMove = -ball.yMove * damping;
-  }
-  ball.yMove += gravity;
-  ball.xPos += ball.xMove;
-  if (((ball.yPos + ball.yMove) + rad) <= c.height) {
-    ball.yPos += ball.yMove;
-  }
-  if ((ball.xPos + ball.xMove + rad > rect.xPos) && (ball.yPos + rad < rect.height)) {
-    ball.xMove = -ball.xMove;
-  }
-  if ((ball.xPos + ball.xMove + rad > rect.xPos) && (ball.yPos + rad < rect.height)) {
-    ball.xMove = -ball.xMove;
-  }
-  if ((ball.xPos + ball.xMove + rad > rect.xPos) && (ball.yPos + rad > rect.yPos)) {
-    ball.xMove = -ball.xMove;
-  }
-  if ((ball.xPos + ball.xMove + rad > rect.xPos) && (ball.yPos + rad > rect.yPos)) {
-    ball.xMove = -ball.xMove;
-  }
-  if ((ball.yPos + ball.yMove + rad > rect.yPos) && (ball.xPos + rad < rect.width + rect.xPos) && (rad + ball.xPos > rect.xPos)) { //collides with the top of the bottom pipe
-    ball.yMove = -ball.yMove;
+  bird.update();//change in the bird or "update"
+  bird.show();
+
+  if (frameCount % 75 == 0) {//this makes pipes appear every so often
+    pipes.push(new Pipe());//new pipe added for every divisible # by 75
   }
 }
-setInterval(draw, 10);
 
-document.addEventListener("keydown", makeBounce);
-function makeBounce(e) {
-  if (e.key == " ") {
-    ball.yMove -= 10;
+function keyPressed() {
+  if (key == ' ') {
+    bird.up();
+    //console.log("SPACE");
   }
-  if (e.key == "r") {
-    ball.xMove = -ball.xMove;
-  }
+}
+function Bird() {
+  this.y = height / 2;
+  this.x = 64;
+
+  this.gravity = 0.7;//gravity is the force that is pushing the bird down
+  this.lift = -12;//spent time tuneing the numbers to make it balance out
+  this.velocity = 0;//gravity is the force that is causing the bird to decrease
+
+  this.show = function() {
+    fill(255);//this is the white rectangles (pipes)
+    ellipse(this.x, this.y, 32, 32);
+  };
+
+  this.up = function() {
+    this.velocity += this.lift; //this creates air volume for the ball and limit speed
+  };
+
+  this.update = function() {
+    this.velocity += this.gravity;//this function will push the bird down
+    // this.velocity *= 0.9;
+    this.y += this.velocity;
+
+    if (this.y > height) {//this checks if the bird is less than the top of screen
+      this.y = height;
+      this.velocity = 0;
+    }
+
+    if (this.y < 0) {
+      this.y = 0;
+      this.velocity = 0;
+    }
+  };
+}
+function Pipe() {
+  this.spacing = 175;
+  this.top = random(height / 6, (3 / 4) * height);//random pipe size generator
+  this.bottom = height - (this.top + this.spacing);
+  this.x = width;//pipe width
+  this.w = 80;
+  this.speed = 6;//pipe speed
+
+  this.highlight = false;
+
+  this.hits = function(bird) {//this will recieve the bird object as its argument
+    if (bird.y < this.top || bird.y > height - this.bottom) {
+      if (bird.x > this.x && bird.x < this.x + this.w) {//checks if it is actually in between the pipes
+        this.highlight = true;//checking if this is actually in the proper area of the pipe
+        return true;//it did return true
+      }
+    }
+    this.highlight = false;//makes pipe turn red after the ball hits pipe
+    return false;//feedback on the result of the code
+  };
+
+  this.show = function() {
+    fill(255);
+    if (this.highlight) {
+      fill(255, 0, 0);
+    }
+    rect(this.x, 0, this.w, this.top);//area of the pipe
+    rect(this.x, height - this.bottom, this.w, this.bottom);
+  };
+
+  this.update = function() {
+    this.x -= this.speed;//changed the speed of the x var
+  };
+
+  this.offscreen = function() {//this creates the offscreen function
+    if (this.x < -this.w) {//all the way off the screen before it goes away
+      return true;//this evaluates the true or false aspect
+    } else {//i found this section of the code online to help make it look more organized
+      return false;
+    }
+  };
 }
